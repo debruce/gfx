3744b70b886e3108a65e4fe284efb7decb5ba1be
David Bruce <david.e.bruce@icloud.com>
2024-08-16 15:53:42 -0700

    blah

--- src/pills.cpp
+++ src/pills.cpp
@@ -5,25 +5,8 @@
 #endif
 
 #include <iostream>
-#include <cxxabi.h>
-#include <memory>
-#include <typeinfo>
 #include <sstream>
 #include <tuple>
-
-template <typename T>
-std::string demangle(T&&) {
-    auto name = typeid(T).name();
-    int status = -4; // some arbitrary value to eliminate the compiler warning
-
-    // enable c++11 by passing the flag -std=c++11 to g++
-    std::unique_ptr<char, void(*)(void*)> res {
-        abi::__cxa_demangle(name, NULL, NULL, &status),
-        std::free
-    };
-
-    return (status==0) ? res.get() : name ;
-}
 
 using namespace std;
 
