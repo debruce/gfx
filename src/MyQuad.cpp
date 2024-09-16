96a57ea38884dc9c5c9e11d6cb6474044e6f8bdc
David Bruce <david.e.bruce@icloud.com>
2024-09-15 20:32:54 -0700

    broken texture setup

--- src/MyQuad.cpp
+++ src/MyQuad.cpp
@@ -1,58 +1,120 @@
 #include "MyQuad.h"
 
-struct SetMyPipelineStates : public vsg::Visitor
+#include <iostream>
+
+std::string VERT{R"(
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(push_constant) uniform PushConstants { mat4 projection; mat4 modelView; };
+
+layout(location = 0) in vec3 vertex;
+layout(location = 1) in vec2 in_texcoord;
+
+out gl_PerVertex { vec4 gl_Position; };
+layout(location = 0) out vec2 out_texcoord;
+
+void main()
+{
+    gl_Position = (projection * modelView) * vec4(vertex, 1.0);
+    out_texcoord = in_texcoord;
+}
+
+)"};
+
+std::string FRAG{R"(
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(location = 0) in vec2 out_texcoord;
+layout(location = 0) out vec4 color;
+layout(set = 0, binding = 0) uniform sampler2D texSampler;
+
+void main()
+{
+    color = texture(texSampler, out_texcoord);
+    // color = vec4(1.0, 0.0, 0.0, 1.0);
+}
+
+)"};
+
+struct SetMyPipelineStates : public vsg::Visitor    // auto indices = ushortArray::create({0, 1, 2, 3, 0});
+
 {
     VkPrimitiveTopology topo;
     const vsg::StateInfo si;
 
-    // explicit SetMyPipelineStates(const vsg::StateInfo& in) :
-    //     si(in) {}
-
     SetMyPipelineStates(const VkPrimitiveTopology& topo, const vsg::StateInfo& si) : topo(topo), si(si){}
 
-    void apply(vsg::Object& object) override
-    {
-        object.traverse(*this);
-    }
-
-    void apply(vsg::RasterizationState& rs) override
-    {
-        // if (si.two_sided)
-        rs.cullMode = VK_CULL_MODE_NONE;
-    }
-
-    void apply(vsg::InputAssemblyState& ias) override
-    {
-        if (si.wireframe) ias.topology = VK_PRIMITIVE_TOPOLOGY_LINE_LIST;
-        else ias.topology = topo;
-    }
-
-    void apply(vsg::ColorBlendState& cbs) override
-    {
-        cbs.configureAttachments(si.blending);
-    }
-
+    void apply(vsg::Object& object) override { object.traverse(*this); }
+    void apply(vsg::RasterizationState& rs) override { rs.cullMode = VK_CULL_MODE_NONE; }
+    void apply(vsg::InputAssemblyState& ias) override { ias.topology = topo; }
+    void apply(vsg::ColorBlendState& cbs) override { cbs.configureAttachments(si.blending); }
 };
 
 using namespace std;
 
-static vsg::vec3 calcNorm(const vsg::vec3& o, const vsg::vec3& a, const vsg::vec3& b)
-{
-    return vsg::normalize(vsg::cross(a - o, b - o));
-}
 
-MyQuad::MyQuad(vsg::ref_ptr<const vsg::Options> options, const vsg::vec3& a, const vsg::vec3& b, const vsg::vec3& c, const vsg::vec3& d)
+MyQuad::MyQuad(vsg::ref_ptr<const vsg::Options> options, const vsg::vec3& a,     // auto indices = ushortArray::create({0, 1, 2, 3, 0});
+const vsg::vec3& b, const vsg::vec3& c, const vsg::vec3& d)
 {
     using namespace vsg;
 
-    if (!_phongShaderSet) _phongShaderSet = createPhongShaderSet(options);
+    auto vertexShader = vsg::ShaderStage::create(VK_SHADER_STAGE_VERTEX_BIT, "main", VERT);
+    auto fragmentShader = vsg::ShaderStage::create(VK_SHADER_STAGE_FRAGMENT_BIT, "main", FRAG);
+    if (!vertexShader || !fragmentShader)
+    {
+        std::cout << "Could not create shaders." << std::endl;
+        exit(-1);
+    }
+    auto shaderSet = vsg::ShaderSet::create(vsg::ShaderStages{vertexShader, fragmentShader});
+    
+    vsg::PushConstantRanges pushConstantRanges{
+        {VK_SHADER_STAGE_VERTEX_BIT, 0, 128} // projection and modelview matrices
+    };
+
+    vsg::VertexInputState::Bindings vertexBindingsDescriptions{
+        VkVertexInputBindingDescription{0, 12, VK_VERTEX_INPUT_RATE_VERTEX}, // interleaved vertex, color, and texcoord data in a single array
+    };
+
+    vsg::VertexInputState::Attributes vertexAttributeDescriptions{
+        VkVertexInputAttributeDescription{0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0},  // vertex data
+        VkVertexInputAttributeDescription{1, 0, VK_FORMAT_R32G32_SFLOAT, 12},    // tex coord data
+    };
+
+    vsg::GraphicsPipelineStates pipelineStates{
+        vsg::VertexInputState::create(vertexBindingsDescriptions, vertexAttributeDescriptions),
+        vsg::InputAssemblyState::create(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST, VK_FALSE),
+        vsg::RasterizationState::create(),
+        vsg::MultisampleState::create(),
+        vsg::ColorBlendState::create(),
+        vsg::DepthStencilState::create()};
+
+    vsg::DescriptorSetLayoutBindings descriptorBindings{
+        {0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT, nullptr} // { binding, descriptorType, descriptorCount, stageFlags, pImmutableSamplers}
+    };
+
+    auto descriptorSetLayout = vsg::DescriptorSetLayout::create(descriptorBindings);
+
+    auto pipelineLayout = vsg::PipelineLayout::create(vsg::DescriptorSetLayouts{descriptorSetLayout}, pushConstantRanges);
+    // auto pipelineLayout = vsg::PipelineLayout::create(vsg::DescriptorSetLayouts{}, pushConstantRanges);
+    auto graphicsPipeline = vsg::GraphicsPipeline::create(pipelineLayout, vsg::ShaderStages{vertexShader, fragmentShader}, pipelineStates);
+    auto bindGraphicsPipeline = vsg::BindGraphicsPipeline::create(graphicsPipeline);
 
     vertices = vec3Array::create({a, b, c, d});
     vertices->properties.dataVariance = vsg::DYNAMIC_DATA;
     auto texcoords = vec2Array::create({vec2{0.0, 0.0}, vec2{1.0, 0.0}, vec2{1.0, 1.0}, vec2{0.0, 1.0}});
-    auto normals = vec3Array::create({calcNorm(a, b, c)});
-    auto colors = vec4Array::create({vec4{1.0, 1.0, 1.0, 1.0}});
-    auto indices = ushortArray::create({0, 2, 1, 2, 3, 0});
+    auto indices = ushortArray::create({0, 2, 1, 0, 3, 2});
+
+    DataList arrays;
+    arrays.push_back(vertices);
+    arrays.push_back(texcoords);
+
+    auto vid = VertexIndexDraw::create();
+    vid->assignArrays(arrays);
+    vid->assignIndices(indices);
+    vid->indexCount = static_cast<uint32_t>(indices->size());
+    vid->instanceCount = 1;
 
     image = ubvec4Array2D::create(512, 512);
     for (auto i = 0; i < 512; i++) {
@@ -67,42 +129,19 @@ MyQuad::MyQuad(vsg::ref_ptr<const vsg::Options> options, const vsg::vec3& a, con
     image->properties.dataVariance = vsg::DYNAMIC_DATA;
     image->dirty();
 
-    DataList arrays;
-    arrays.push_back(vertices);
-    arrays.push_back(normals);
-    arrays.push_back(texcoords);
-    arrays.push_back(colors);
-    // if (positions) arrays.push_back(positions);
+    auto sampler = vsg::Sampler::create();
+    sampler->addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+    sampler->addressModeV = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
 
-    auto vid = VertexIndexDraw::create();
-    vid->assignArrays(arrays);
-    vid->assignIndices(indices);
-    vid->indexCount = static_cast<uint32_t>(indices->size());
-    vid->instanceCount = 1;
-
-    auto gpConf = vsg::GraphicsPipelineConfigurator::create(_phongShaderSet);
+    auto descriptorImage = vsg::DescriptorImage::create(sampler, image, 0, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+    auto descriptorSet = vsg::DescriptorSet::create(descriptorSetLayout, vsg::Descriptors{descriptorImage});
+    auto bindDescriptorSet = vsg::BindDescriptorSet::create(VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline->layout, 0, descriptorSet);
 
     stateGroup = vsg::StateGroup::create();
 
-    auto sampler = Sampler::create();
-    sampler->addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
-    sampler->addressModeV = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
-    gpConf->assignTexture("diffuseMap", image, sampler);
-    // gpConf->enableTexture("diffuseMap");
-    auto mat = vsg::PhongMaterialValue::create();
-    mat->value().diffuse.set(1.0f, 1.0f, 1.0f, 1.0f);
-    mat->value().specular.set(0.0f, 0.0f, 0.0f, 1.0f);
-    gpConf->assignDescriptor("material", mat);
-
-    vsg::StateInfo si;
-    SetMyPipelineStates sps(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST, si);
-    gpConf->accept(sps);
-    gpConf->init();
-
-
-    gpConf->copyTo(stateGroup);
-
+    stateGroup->addChild(bindGraphicsPipeline);
     stateGroup->addChild(vid);
+    stateGroup->addChild(bindDescriptorSet);
 
     addChild(stateGroup);
 }
